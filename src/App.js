72159c3abe235fc92ecb450b554222c13a500391
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-10 12:11:26 -0800

    imported other comps, writing js, accessing data from other comps(props)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+// Import Component
+import Tweet from "./Components/Tweet"
+import CreateTweet from './Components/CreateTweet';
 
 function App() {
+  // Here is where we can write JS
+  const name = "Dominic";
+  const message = "I love coffee"
   return (
     <div>
       <h1>Hello React</h1>
+      <CreateTweet />
+      <Tweet author={name} message={message}/>
     </div>
   );
 }
