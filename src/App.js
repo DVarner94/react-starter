4ad283fc10dbc7ea666fa03fcc707f944ad6d0b1
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-13 11:25:41 -0800

    delete useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 // Import Components
 import TweetList from './Components/TweetList';
 import CreateTweet from './Components/CreateTweet';
